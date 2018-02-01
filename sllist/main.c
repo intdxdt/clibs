8d02590057adc99b009880ccf006670d2411d9ff
Titus Tienaah <titus.intdxdt@gmail.com>
2018-02-01 07:09:25 -0400

    slist

--- sllist/main.c
+++ sllist/main.c
@@ -2,5 +2,11 @@
 #include "sllist.h"
 
 int main() {
+    Node *root = NULL;
+    ssl_insert(&root, 3);
+    ssl_insert(&root, 2);
+    ssl_insert(&root, 5);
+    ssl_insert(&root, 1);
+    ssl_insert(&root, 0);
     return 0;
 }
