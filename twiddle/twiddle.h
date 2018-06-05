d3d8b4eeddbea5545962e183a3ce0d4c3e8040b2
Titus Tienaah <titus.intdxdt@gmail.com>
2018-06-04 23:52:45 -0300

    twiddle

--- twiddle/twiddle.h
+++ twiddle/twiddle.h
@@ -1,24 +1,18 @@
-//
-// Created by titus on 10/25/17.
-//
-
-#ifndef ROBUSTCPP_TWIDDLE_H
-#define ROBUSTCPP_TWIDDLE_H
-
 #include <cstdint>
 
-
+#ifndef TWIDDLE_CPP_H
+#define TWIDDLE_CPP_H
 /**
  * Based on Stanford bit twiddling hack library
  *    http//graphics.stanford.edu/~seander/bithacks.html
  */
-typedef uint32_t uint32;
-typedef int32_t int32;
-const uint32 INT_BITS = 32; //number of bits in an integer
-const uint32 INT_MAX = 0x7fffffff;
-const uint32 INT_MIN = static_cast<const uint32>(-1 << uint32(INT_BITS - 1));
+using uint32 =  uint32_t;
+using int32  =  int32_t;
+//const uint32 INT_BITS = 32; //number of bits in an integer
+//const int32 INT_MAX = std::numeric_limits<int>::max();
+//const int32 INT_MIN = std::numeric_limits<int>::min();
 //@formatter:off
-unsigned char REVERSE_TABLE[256] = {0, 128, 64, 192, 32, 160, 96, 224, 16, 144, 80, 208, 48,
+const uint32 REVERSE_TABLE[256] = {0, 128, 64, 192, 32, 160, 96, 224, 16, 144, 80, 208, 48,
 	176, 112, 240, 8, 136, 72, 200, 40, 168, 104, 232, 24, 152, 88, 216, 56, 184, 120, 248, 4,
 	132, 68, 196, 36, 164, 100, 228, 20, 148, 84, 212, 52, 180, 116, 244, 12, 140, 76, 204, 44,
 	172, 108, 236, 28, 156, 92, 220, 60, 188, 124, 252, 2, 130, 66, 194, 34, 162, 98, 226, 18,
@@ -34,32 +28,13 @@ unsigned char REVERSE_TABLE[256] = {0, 128, 64, 192, 32, 160, 96, 224, 16, 144, 
 	191, 127, 255};
 //@formatter:on
 
-//bool to int32
-int32 inline bint32(bool b) {
-    return b ? 1 : 0;
-}
 
-//bool to uint32
-uint32 buint32(bool b) {
-    if (b) {
-        return 1;
-    }
-    return 0;
-}
-
-//uint32 to int
-int usz(uint32 v) { return int(v); }
 
 //Returns -1, 0, +1 depending on sign of x
-int32 sign(int32 v) {
-    return bint32(v > 0) - bint32(v < 0);
+inline int32 sign(int32 v) {
+    return (v > 0) - (v < 0);
 }
 
-//Computes absolute value of integer
-int32 abs(int32 v) {
-    auto mask = v >> uint32(INT_BITS - 1);
-    return (v ^ mask) - mask;
-}
 
 //Checks if a number is a power of two
 bool inline is_pow2(int32 v) {
@@ -67,31 +42,38 @@ bool inline is_pow2(int32 v) {
 }
 
 //Computes log base 2 of v
-uint32  inline log2(uint32 v) {
+inline uint32 log2(uint32 v) {
     uint32 r{0}, shift{0};
     //@formatter:off
-	r = buint32(v > 0xFFFF) << 4;   v >>= r;
-	shift = buint32(v > 0xFF) << 3; v >>= shift; r |= shift;
-	shift = buint32(v > 0xF)  << 2; v >>= shift; r |= shift;
-	shift = buint32(v > 0x3)  << 1; v >>= shift; r |= shift;
-	return r | (v >> 1);
-	//@formatter:on
+    r = static_cast<uint32>((v > 0xFFFF) << 4u);
+    v >>= r;
+    shift = static_cast<uint32>((v > 0xFF) << 3u);
+    v >>= shift;
+    r |= shift;
+    shift = static_cast<uint32>((v > 0xF) << 2u);
+    v >>= shift;
+    r |= shift;
+    shift = static_cast<uint32>((v > 0x3) << 1u);
+    v >>= shift;
+    r |= shift;
+    return r | (v >> 1u);
+    //@formatter:on
 
 }
 
 //Computes log base 10 of v
 int32 log10(int32 v) {
     //@formatter:off
-	return  (v >= 1000000000)	? 9:
-			(v >= 100000000) 	? 8:
-			(v >= 10000000)  	? 7:
-			(v >= 1000000)		? 6:
-			(v >= 100000) 		? 5:
-			(v >= 10000)  		? 4:
-			(v >= 1000)     	? 3:
-			(v >= 100) 	    	? 2:
-			(v >= 10)       	? 1:0;
-	//@formatter:on
+    return (v >= 1000000000) ? 9 :
+           (v >= 100000000) ? 8 :
+           (v >= 10000000) ? 7 :
+           (v >= 1000000) ? 6 :
+           (v >= 100000) ? 5 :
+           (v >= 10000) ? 4 :
+           (v >= 1000) ? 3 :
+           (v >= 100) ? 2 :
+           (v >= 10) ? 1 : 0;
+    //@formatter:on
 }
 
 //Counts number of bits
@@ -110,6 +92,7 @@ uint32 pop_count(uint32 v) {
     c = ((c >> s3) + c) & b3;
     return ((c >> s4) + c) & b4;
 }
+
 //
 ////finding the log base 2 in parallel
 ////first isolate the lowest 1 bit, and then
@@ -128,69 +111,69 @@ uint32 pop_count(uint32 v) {
 ////}
 ////Computes the number of trailing zeros by accumulating c
 //// in a manner akin to binary search
-//func CountTrailingZeros(v uint32) uint32 {
-//	var c uint32 = 32
-//	// NOTE if 0 == v, then c = 31.
-//	//short circuit v == 0 as 32
-//	if v == 0 {
-//		return c
-//	}
-//	if v&0x1 != 0 {
-//		c = 0 // special case for odd v (assumed to happen half of the time)
-//	} else {
-//		c = 1
-//		if (v & 0xffff) == 0 {
-//			v >>= 16
-//			c += 16
-//		}
-//		if (v & 0xff) == 0 {
-//			v >>= 8
-//			c += 8
-//		}
-//		if (v & 0xf) == 0 {
-//			v >>= 4
-//			c += 4
-//		}
-//		if (v & 0x3) == 0 {
-//			v >>= 2
-//			c += 2
-//		}
-//		c -= v & 0x1
-//	}
-//	return c
-//}
-//
+uint32 count_trailing_zeros(uint32 v) {
+    uint32 c = 32;
+    // NOTE if 0 == v, then c = 31.
+    //short circuit v == 0 as 32
+    if (v == 0) {
+        return c;
+    }
+    if ((v & 0x1u) != 0) {
+        c = 0; // special case for odd v (assumed to happen half of the time)
+    } else {
+        c = 1;
+        if ((v & 0xffffu) == 0) {
+            v >>= 16;
+            c += 16;
+        }
+        if ((v & 0xffu) == 0) {
+            v >>= 8;
+            c += 8;
+        }
+        if ((v & 0xfu) == 0) {
+            v >>= 4;
+            c += 4;
+        }
+        if ((v & 0x3u) == 0) {
+            v >>= 2;
+            c += 2;
+        }
+        c -= v & 0x1u;
+    }
+    return c;
+}
+
 ////Rounds to next power of 2
-//func NextPow2(v int32) int32 {
-//	v += bint32(v == 0)
-//	v -= 1
-//	v |= v >> 1
-//	v |= v >> 2
-//	v |= v >> 4
-//	v |= v >> 8
-//	v |= v >> 16
-//	return v + 1
-//}
-//
+int32 next_pow2(uint32 v) {
+    v += (v == 0);
+    v -= 1;
+    v |= v >> 1u;
+    v |= v >> 2u;
+    v |= v >> 4u;
+    v |= v >> 8u;
+    v |= v >> 16u;
+    return v + 1u;
+}
+
 ////Rounds down to previous power of 2
-//func PrevPow2(v int32) int32 {
-//	v |= v >> 1
-//	v |= v >> 2
-//	v |= v >> 4
-//	v |= v >> 8
-//	v |= v >> 16
-//	return v - (v >> 1)
-//}
-//
+int32 prev_pow2(uint32 v) {
+    v |= v >> 1u;
+    v |= v >> 2u;
+    v |= v >> 4u;
+    v |= v >> 8u;
+    v |= v >> 16u;
+    return v - (v >> 1u);
+}
+
 ////Computes parity of word
-//func Parity(v int32) int32 {
-//	v ^= v >> 16
-//	v ^= v >> 8
-//	v ^= v >> 4
-//	v &= 0xf
-//	return (0x6996 >> uint32(v)) & 1
-//}
-//
+int32 parity(uint32 v) {
+    v ^= v >> 16u;
+    v ^= v >> 8u;
+    v ^= v >> 4u;
+    v &= 0xf;
+    return (0x6996u >> v) & 1u;
+}
+
 ////func _gen_reverse_table(table&mut [int32256]) {
 ////  for i in  0..256 {
 ////    var   v = i
@@ -208,33 +191,33 @@ uint32 pop_count(uint32 v) {
 ////}
 //
 ////Reverse bits in a 32 bit word
-//func Reverse(v uint32) uint32 {
-//	//@formatter:off
-//	return (REVERSE_TABLE[usz(v & 0xff)] << 24) |
-//		(REVERSE_TABLE[usz((v>>8)&0xff) ] << 16) |
-//		(REVERSE_TABLE[usz((v>>16)&0xff) ] << 8) |
-//		REVERSE_TABLE[usz((v>>24)&0xff) ]
-//	//@formatter:on
-//}
-//
+uint32 reverse(uint32 v) {
+    //@formatter:off
+    return (REVERSE_TABLE[v  & 0xffu      ] << 24u) |
+           (REVERSE_TABLE[v >> 8u  & 0xffu] << 16u) |
+           (REVERSE_TABLE[v >> 16u & 0xffu] << 8u)  |
+           REVERSE_TABLE [v >> 24u & 0xffu];
+    //@formatter:on
+}
+
 ////Interleave bits of 2 coordinates with 16
 //// Useful for fast quadtree codes
-//func Interleave2(x uint32, y uint32) uint32 {
-//	x &= 0xFFFF
-//	x = (x | (x << 8)) & 0x00FF00FF
-//	x = (x | (x << 4)) & 0x0F0F0F0F
-//	x = (x | (x << 2)) & 0x33333333
-//	x = (x | (x << 1)) & 0x55555555
-//
-//	y &= 0xFFFF
-//	y = (y | (y << 8)) & 0x00FF00FF
-//	y = (y | (y << 4)) & 0x0F0F0F0F
-//	y = (y | (y << 2)) & 0x33333333
-//	y = (y | (y << 1)) & 0x55555555
-//
-//	return x | (y << 1)
-//}
-//
+uint32 interleave2(uint32 x, uint32 y) {
+    x &= 0xFFFF;
+    x = (x | (x << 8u)) & 0x00FF00FFu;
+    x = (x | (x << 4u)) & 0x0F0F0F0Fu;
+    x = (x | (x << 2u)) & 0x33333333u;
+    x = (x | (x << 1u)) & 0x55555555u;
+
+    y &= 0xFFFF;
+    y = (y | (y << 8u)) & 0x00FF00FFu;
+    y = (y | (y << 4u)) & 0x0F0F0F0Fu;
+    y = (y | (y << 2u)) & 0x33333333u;
+    y = (y | (y << 1u)) & 0x55555555u;
+
+    return x | (y << 1u);
+}
+
 ////Extracts the nth interleaved component
 //func Deinterleave2(v uint32, n uint32) uint32 {
 //	//@formatter:off
