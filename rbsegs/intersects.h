4ad944d3dba2f9968a645018b93cba3a9445d7d0
Titus Tienaah <titus.intdxdt@gmail.com>
2018-08-11 07:57:53 -0300

    intersects

--- rbsegs/intersects.h
+++ rbsegs/intersects.h
@@ -2,6 +2,7 @@
 // Created on 11/08/18.
 //
 #include <vector>
+#include <array>
 #include "util.h"
 #include "../mbr/mbr.h"
 #include "../mutil/mutil.h"
@@ -24,30 +25,14 @@ inline double snap_to_zero_or_one(double v) {
     return feq(v, 0.0) ? 0.0 : feq(v, 1.0) ? 1.0 : v;
 }
 
-std::tuple<double, double, double> segseg_abd(const Pt& sa, const Pt& sb, const Pt& oa, const Pt& ob) {
-    double x1, y1, x2, y2, x3, y3, x4, y4, d, a, b;
-
-    x1 = sa[x], y1 = sa[y];
-    x2 = sb[x], y2 = sb[y];
-
-    x3 = oa[x], y3 = oa[y];
-    x4 = ob[x], y4 = ob[y];
-
-    d = ((y4 - y3) * (x2 - x1)) - ((x4 - x3) * (y2 - y1));
-    a = ((x4 - x3) * (y1 - y3)) - ((y4 - y3) * (x1 - x3));
-    b = ((x2 - x1) * (y1 - y3)) - ((y2 - y1) * (x1 - x3));
-
-    return std::tuple<double, double, double>(a, b, d);
-}
 
 //do two lines intersect line segments a && b with
 //vertices sa, sb, oa, ob
-bool intersects( const Pt& sa,  const Pt& sb,  const Pt& oa,  const Pt& ob) {
+bool intersects(const Pt& sa, const Pt& sb, const Pt& oa, const Pt& ob) {
     auto bln = false;
-    auto abd = segseg_abd(sa, sb, oa, ob);
-    auto a = std::get<0>(abd);
-    auto b = std::get<1>(abd);
-    auto d = std::get<2>(abd);
+    auto a = ((ob[0] - oa[0]) * (sa[1] - oa[1])) - ((ob[1] - oa[1]) * (sa[0] - oa[0]));
+    auto b = ((sb[0] - sa[0]) * (sa[1] - oa[1])) - ((sb[1] - sa[1]) * (sa[0] - oa[0]));
+    auto d = ((ob[1] - oa[1]) * (sb[0] - sa[0])) - ((ob[0] - oa[0]) * (sb[1] - sa[1]));
 
     //snap to zero if near -0 or 0
     a = snap_to_zero(a);
