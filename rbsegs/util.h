28b8231f5b95d9b82411deacc5b0bc9e98baa7aa
Titus Tienaah <titus.intdxdt@gmail.com>
2018-08-10 23:50:05 -0300

    rbsegs

---
+++ rbsegs/util.h
@@ -0,0 +1,17 @@
+//
+// Created by resson on 10/08/18.
+//
+
+#ifndef RBSEGS_UTIL_H
+#define RBSEGS_UTIL_H
+
+template<typename T>
+inline const T& min(const T& a, const T& b) {
+    return b < a ? b : a;
+};
+
+template<typename T>
+inline const T& max(const T& a, const T& b) {
+    return b > a ? b : a;
+};
+#endif //RBSEGS_UTIL_H
